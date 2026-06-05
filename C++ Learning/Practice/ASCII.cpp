9bc12040fe5eb1f9af99c75a12372b37c8768e3d
Chitransh07g <chitransh.k.g.001@gmail.com>
2026-06-05 15:12:28 +0530

    ASCII and Alpha

---
+++ C++ Learning/Practice/ASCII.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+int main(){
+char x;
+
+std::cout<<"to know the ASCII code of the alphabhet enter the letter \n  ";
+std::cin>>x;
+
+int n = x;
+std::cout<<"\n"<< n;
+}
---
+++ C++ Learning/Practice/Alpha.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"to know the ASCII code of the alphabet enter the letter: ";
+    int c = getchar();
+    cout<<"\nASCII code: "<<c<<endl;
+    return 0;
+}
